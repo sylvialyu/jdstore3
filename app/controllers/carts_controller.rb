7cd9afc0675fdb5cab1e4bf8331c87ece168456d
sylvia.lyu <hisylvia.lyu@gmail.com>
2017-07-17 12:23:05 +0800

    empty cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,4 +1,10 @@
 class CartsController < ApplicationController
 
-  
+  def clean
+    @cart = current_cart
+    @cart.clean!
+    redirect_to carts_path
+    flash[:warning] = "已清空购物车"
+  end
+
 end
