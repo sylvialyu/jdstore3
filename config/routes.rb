dfd5f0c72df02d446420a793daaa12d7115675f2
sylvia.lyu <hisylvia.lyu@gmail.com>
2017-07-18 16:43:41 +0800

    add admin/orders/state_option button

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,14 @@ Rails.application.routes.draw do
 
   namespace :admin do
     resources :products
-    resources :orders
+    resources :orders do
+      member do
+        post :ship
+        post :shipped
+        post :return
+        post :cancel
+      end
+    end
   end
 
   resources :carts do
